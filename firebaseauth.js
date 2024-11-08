e69b9997120437939af00d5228e3de8317129ab5
Alopes91 <anderson91lopes@gmail.com>
2024-11-07 22:22:07 -0300

    login com google
    login com google - parte I

--- firebaseauth.js
+++ firebaseauth.js
@@ -16,6 +16,29 @@ const firebaseConfig = {
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
+// Login com Conta Google
+const auth = getAuth();
+signInWithPopup(auth, provider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+    });
+
+
 // Função para exibir mensagens temporárias na interface
 function showMessage(message, divId) {
     var messageDiv = document.getElementById(divId);
